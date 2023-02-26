2ff0e1c391198b38ab989544131593b4558e1c4e
Sayantan-Bhowmick <sayantanbhowmick27523@gmail.com>
2023-02-26 23:56:05 +0530

    added quiz

--- src/components/UI/Banner/banner.jsx
+++ src/components/UI/Banner/banner.jsx
@@ -4,6 +4,7 @@ import Card_2 from '../Card/card_2';
 import Card_3 from '../Card/card_3';
 import Card_4 from '../Card/card_4';
 import Card_5 from '../Card/card_5';
+import './banner.css';
 
 function Banner() {
   const [start, setIsStart] = useState(false);
@@ -16,20 +17,20 @@ function Banner() {
   const renderBanner = () => {
     return(
       <div>
-        <h1>Quizz App</h1>
-        <button onClick={handleStartClick}>Start Quiz</button>
+        <h1 className='bannerHead'>Quizz App</h1>
+        <button onClick={handleStartClick} className='bannerButton'>Start Quiz</button>
       </div>
     );
   }
   const renderCards = () => {
     return(
       <>
-        <h1>Quizz App</h1>
-        <Card_1></Card_1>
-        <Card_2></Card_2>
-        <Card_3></Card_3>
-        <Card_4></Card_4>
-        <Card_5></Card_5>
+        <h1 className='bannerHead'>Quizz App</h1>
+        <div className='cardTop_1'><Card_1/></div>
+        <div className='cardTop_2'><Card_2/></div>
+        <div className='cardBottom_1'><Card_3/></div>
+        <div className='cardBottom_2'><Card_4/></div>
+        <div className='cardBottom_3'><Card_5/></div>
       </>
     );
   }
